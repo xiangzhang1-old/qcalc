a045c51c5b12970370a96bdd528e3dac253f147c
Xiang Zhang <xzhang1@mit.edu>
2019-03-05 08:45:12 -0500

    .

--- __init__.py
+++ __init__.py
@@ -4,19 +4,20 @@
 # 不重要
 
 import pandas as pd, os, unicodedata, re, collections, subprocess
+import functools
 import ase, ase.io
 
 LIB_PATH = os.path.dirname(os.path.realpath(__file__))
 POTCAR_PATH = "/home/xzhang1/src/VASP_PSP/potpaw_PBE.54/"
 
-def _periodic_table_lookup(symbol, column, periodic_table = pd.read_excel(LIB_PATH + '/periodic_table.xlsx')):
+def periodic_table_lookup(symbol, column, periodic_table = pd.read_excel(LIB_PATH + '/periodic_table.xlsx')):
     """
     :param str symbol: 'Pb'
     :param str column: 'pot_encut'
     """
     return periodic_table.loc[periodic_table.symbol == symbol, column].values[0]
 
-def _slugify(value):
+def slugify(value):
     """
     Make a string URL- and filename-friendly.
     Taken from django/utils/text.py. In Django, a "slug" is a URL- and filename-friendly string.
@@ -26,7 +27,7 @@ def _slugify(value):
     :rtype: unicode
     :raises TypeError: if value is not unicode string
     """
-    value = unicodedata.normalize('NFKD', value).encode('ascii', 'ignore')
+    value = unicodedata.normalize('NFKD', value)
     value = re.sub(r'[^\w\s-]', '', value).strip().lower()
     value = re.sub(r'[-\s]+', '-', value)
     return value
@@ -60,49 +61,62 @@ class Struct(object):
 
 
 """考虑单电子单核解。考虑多电子多核解。考虑其近似最小化问题。考虑近似：材料，求值模式，简化近似，辅助行为。"""
-# class dict:
-# 考察 args, kwargs
-# - args实现为args=True
-# - _ 变量名前缀表示"隐藏"。Alternatively,
+# 考虑 args, kwargs 的规则变换，实现为 exec + locals。改：
+# - args 实现为 opt = True
+# - hidden = {'hidden'} 不 print
+# - exec 后确认没有覆盖
+# - exec 文件用 # 分块，来回走三趟。
 
-class Getopt(collections.MutableMapping):
+# d = {}
+
+def exec_block_raise(s, d):
     """
-    考察 args, kwargs 的规则变换，规则为 python改：[2,3]
-    - args实现为args=True
-    - _ 变量名前缀表示"隐藏"，__getitem__时一并搜索。
-    - __getitem__未搜索到时返回None。
-    - __setitem__时禁止覆盖。
+    Executes s, write variable to dictionary d, no overwrite.
+
+    :param str s: multi-line code block to be executed
+    :param dict d: args-kwargs to be updated
     """
-    def __init__(self):
-        self._dict = dict()
+    old = d.copy()
+    exec(s, globals(), d)
+    assert old.items() <= d.items()
 
-    def __getitem__(self, key):
-        if key in self._dict:
-            return self._dict[key]
-        if key.startswith('_') and key[1:] in self._dict:
-            return self._dict[key[1:]]
-        return None
+def exec_block_ignore(s, d):
+    """
+    Same as exec_block_raise, except Overwrite, NameError, AssertionError are silently ignored.
+    """
+    try:
+        exec_block_raise(s, d)
+    except (AssertionError, NameError) as e:
+        pass
 
-    def __setitem__(self, key, value):
-        if key in self._dict and self._dict[key] != value:
-            raise SyntaxError("overwrite")
-        self._dict[key] = value
+def exec_shortargs_hideargs_noquote(s, d):  # helper
+    """
+    Convenience function. Accepts 'opt' and 'spin=fm', hides opt, evaluates [1,2,3] and unquoted string. No overwrite.
+    """
+    old = d.copy()
+    if '=' not in s:                            # opt
+        assert s == slugify(s)                  # is clean
+        d[s] = True
+        d['hidden'].add(s)                      # hides opt
+    elif '=' in s:                              # spin=fm
+        l, r = s.split('=')
+        assert l == slugify(l)
+        try:
+            d[l] = eval(r)                      # evaluates [1,2,3]
+        except NameError:
+            d[l] = r                            # unquoted string
+    assert old.items() <= d.items()             # no overwrite
 
-    def __delitem__(self, key):
-        raise SyntaxError("overwrite")
 
-    def __iter__(self):
-        return iter(self._dict)
 
-    def __len__(self):
-        return len(self._dict)
 
-    def exec(self, expr):
-        exec(expr, globals(), self)
+
+
+
 
 # getopt.exec("pbs=qd=True; opt=True; ediffg=1E-3")
 #
-# with open("getopt.vasp", "r") as file:     # if opt:   ediff=1E-4  # bourbaki
+# with open("getopt.vasp.py", "r") as file:     # if opt:   ediff=1E-4  # bourbaki
 #     for line in file:
 #         getopt.exec(line)
 
@@ -126,7 +140,7 @@ def _struct_to_potcar(struct):
     :return: writes POTCAR to current directory
     """
     for symbol in struct.stoichiometry:
-        fp = POTCAR_PATH + _periodic_table_lookup(symbol, "pot") + "/POTCAR"
+        fp = POTCAR_PATH + periodic_table_lookup(symbol, "pot") + "/POTCAR"
         # we will repeatedly use this trick: str.format(dict) and f"{var}"
         subprocess.run(f"cat {fp} >> POTCAR", shell=True)
 
