04544be5ec4f7a4f9c58627a7aec4955361351d4
Xiang Zhang <xzhang1@mit.edu>
2019-03-25 20:38:47 -0400

    type

--- __init__.py
+++ __init__.py
@@ -62,7 +62,8 @@ class Struct(object):
 hidden = {'hidden'}：非官方 kwargs
 phi0 = path, rho0 = 0, rho = path：KS波函数、电荷密度的初值
 kpoints = [template, param, ...]：KPOINTS 定式与参数
-path：文件夹路径
+path：文件夹路
+type = vasp：
 """
 class D(collections.MutableMapping):
     """
