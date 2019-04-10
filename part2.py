6a3557d068c3dfd8c8afa1a779d94ff5c87b01a3
Xiang Zhang <xzhang1@mit.edu>
2019-04-10 17:07:32 -0400

    .

--- part2.py
+++ part2.py
@@ -1,93 +1,46 @@
-import unicodedata, re
-
-def exec_short(s, d):  # helper
-    """
-    Convenience function. Accepts 'opt' and 'spin=fm', hides opt, evaluates [1,2,3] and unquoted string. No overwrite.
-    """
-    old = d.copy()
-    if '=' not in s:                            # opt
-        s = slugify(s)                          # clean
-        d[s] = True
-        d['hidden'].add(s)                      # hides opt
-    elif '=' in s:                              # spin=fm
-        l, r = slugify(l), slugify(r)
-        assert l == slugify(l)
-        try:
-            d[l] = eval(r)                      # evaluates [1,2,3]
-        except NameError:
-            d[l] = r                            # unquoted string
-    assert old.items() <= d.items()             # no overwrite
-
-def slugify(value):
-    """
-    Make a string URL- and filename-friendly.
-    Taken from django/utils/text.py. In Django, a "slug" is a URL- and filename-friendly string.
-
-    :param unicode value: string to be converted
-    :return: filename-friendly string
-    :rtype: unicode
-    :raises TypeError: if value is not unicode string
-    """
-    value = unicodedata.normalize('NFKD', value)
-    value = re.sub(r'[^\w\s-]', '', value).strip().lower()
-    value = re.sub(r'[-\s]+', '-', value)
-    return value
-
-
 # ----------------------------------------------------------------------------------------------------------------------
-def uuid4():
+# vasp(d, struct)
+
+def d_struct_to_vasp(d, struct):
     """
-    Generates a random UUID.UUID4 encoded in base57.
-    Taken from shortuuid.
-    Encodes a UUID into a string (LSB first) according to the alphabet. If leftmost (MSB) bits 0, string might be shorter
+    输出文本文件: INCAR, POSCAR4, KPOINTS, POTCAR, CHGCAR/WAVECAR
+    :param dict d:
+    :param Struct struct:
+    :return: converts d, struct to VASP files (INCAR, POSCAR, KPOINTS, POTCAR) in current directory
     """
-    alphabet = list("23456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz")
-    unique_id = uuid.uuid4().int
-    output = ""
-    while unique_id:
-        unique_id, digit = divmod(unique_id, len(alphabet))
-        output += alphabet[digit]
-    return output
-
-uuid_object = pd.DataFrame(columns=['uuid', 'object'])                      # 关系 (uuid, object)
-from_to = pd.DataFrame(columns=['from', 'to'])                              # 关系 (uuid "from", uuid "to")
-parent_child = pd.DataFrame(columns=['parent', 'child'])                    # 关系 (uuid "parent", uuid "child")
-original_doppelganger = pd.DataFrame(columns=['original', 'doppelganger'])  # 关系 (uuid "original", uuid "doppelganger")
-
-def object2s(relation, column1, object1, column2):
-    # 求所有 object2 使得 relation(column1 = object1, column2 = object2) 成立
-    uuid1 = uuid_object.query("object = @object1").uuid.item()
-    uuid2 = relation.query(f"{column1} = {uuid1}")[column2].item()
-    object2s = uuid_object.query(f"uuid = {uuid2}").object
-    return object2s
-
-# ----------------------------------------------------------------------------------------------------------------------
-# bash, python 一轮一轮。prev, sinfo, sbatch, rsync。
-# 常见代码块的自动化
-
-
-status = [0, 1, ..., 'Complete', 'Error']
-
-# optionally
-def is_prev_status_complete():
-def ready1(d, struct):
-    return 前节点已完成
-
-def run1(d, struct):
+    with open("INCAR", "w") as file:
+        for k, v in d.items():
+            if k not in d['hidden']:
+                file.write("{k} = {v}\n")
     #
-    d.exec_file("d.exec.vasp.py")
+    atoms = ase.Atoms(symbols=struct.X['symbol'], positions=struct.X[['x', 'y', 'z']], cell=struct.A)
+    ase.io.write("POSCAR", images=atoms, format="vasp")
     #
-    d_struct_to_vasp(d, struct)
-    d_to_slurm(d)
+    template(i = f"{LIB_PATH}/KPOINTS.{d['kpoints'][0]}", o = "KPOINTS", d = d)
     #
-    subprocess.run("submit")
+    for symbol in struct.stoichiometry:
+        potcar = POTCAR_PATH + periodic_table_lookup(symbol, "pot") + "/POTCAR"
+        subprocess.run(f"cat {potcar} >> POTCAR", shell=True)
+    #
+    for path in [d[k] for k in ['rho', 'rho0', 'phi0'] if k in d]:
+        subprocess.run(f"rsync -a -h --info=progress2 {path} .", shell=True)
 
 # ----------------------------------------------------------------------------------------------------------------------
+# slurm(d)
 
-# ----------------------------------------------------------------------------------------------------------------------
-def suggest_host():
-    pass
+def submit(d):
+    template(i = f"{LIB_PATH}/submit.{d['transforms'][0]}.{d['host']}", o = "submit", d = d)
+    template(i = f"{LIB_PATH}/job.{d['transforms'][0]}.{d['host']}", o = "job", d = d)
+    subprocess.run("bash submit", shell=True)
 
-# ----------------------------------------------------------------------------------------------------------------------
-# plugin: 自动继承 struct，自动覆盖 phi0, rho0, rho
+def is_complete_on_slurm(d):
+    template(i=f"{LIB_PATH}/is_complete_on_slurm.{d['host']}", o="is_complete", d = d)
+    return eval(subprocess.check_output("bash is_complete", shell=True))
+
+def retrieve(d):
+    template(i=f"{LIB_PATH}/retrieve.{d['host']}", o="retrieve", d = d)
+    subprocess.run("bash retrieve", shell=True)
+
+
+
 
---
+++ part3.py
@@ -0,0 +1,93 @@
+import unicodedata, re
+
+def exec_short(s, d):  # helper
+    """
+    Convenience function. Accepts 'opt' and 'spin=fm', hides opt, evaluates [1,2,3] and unquoted string. No overwrite.
+    """
+    old = d.copy()
+    if '=' not in s:                            # opt
+        s = slugify(s)                          # clean
+        d[s] = True
+        d['hidden'].add(s)                      # hides opt
+    elif '=' in s:                              # spin=fm
+        l, r = slugify(l), slugify(r)
+        assert l == slugify(l)
+        try:
+            d[l] = eval(r)                      # evaluates [1,2,3]
+        except NameError:
+            d[l] = r                            # unquoted string
+    assert old.items() <= d.items()             # no overwrite
+
+def slugify(value):
+    """
+    Make a string URL- and filename-friendly.
+    Taken from django/utils/text.py. In Django, a "slug" is a URL- and filename-friendly string.
+
+    :param unicode value: string to be converted
+    :return: filename-friendly string
+    :rtype: unicode
+    :raises TypeError: if value is not unicode string
+    """
+    value = unicodedata.normalize('NFKD', value)
+    value = re.sub(r'[^\w\s-]', '', value).strip().lower()
+    value = re.sub(r'[-\s]+', '-', value)
+    return value
+
+
+# ----------------------------------------------------------------------------------------------------------------------
+def uuid4():
+    """
+    Generates a random UUID.UUID4 encoded in base57.
+    Taken from shortuuid.
+    Encodes a UUID into a string (LSB first) according to the alphabet. If leftmost (MSB) bits 0, string might be shorter
+    """
+    alphabet = list("23456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz")
+    unique_id = uuid.uuid4().int
+    output = ""
+    while unique_id:
+        unique_id, digit = divmod(unique_id, len(alphabet))
+        output += alphabet[digit]
+    return output
+
+uuid_object = pd.DataFrame(columns=['uuid', 'object'])                      # 关系 (uuid, object)
+from_to = pd.DataFrame(columns=['from', 'to'])                              # 关系 (uuid "from", uuid "to")
+parent_child = pd.DataFrame(columns=['parent', 'child'])                    # 关系 (uuid "parent", uuid "child")
+original_doppelganger = pd.DataFrame(columns=['original', 'doppelganger'])  # 关系 (uuid "original", uuid "doppelganger")
+
+def object2s(relation, column1, object1, column2):
+    # 求所有 object2 使得 relation(column1 = object1, column2 = object2) 成立
+    uuid1 = uuid_object.query("object = @object1").uuid.item()
+    uuid2 = relation.query(f"{column1} = {uuid1}")[column2].item()
+    object2s = uuid_object.query(f"uuid = {uuid2}").object
+    return object2s
+
+# ----------------------------------------------------------------------------------------------------------------------
+# bash, python 一轮一轮。prev, sinfo, sbatch, rsync。
+# 常见代码块的自动化
+
+
+status = [0, 1, ..., 'Complete', 'Error']
+
+# optionally
+def is_prev_status_complete():
+def ready1(d, struct):
+    return 前节点已完成
+
+def run1(d, struct):
+    #
+    d.exec_file("d.exec.vasp.py")
+    #
+    d_struct_to_vasp(d, struct)
+    d_to_slurm(d)
+    #
+    subprocess.run("submit")
+
+# ----------------------------------------------------------------------------------------------------------------------
+
+# ----------------------------------------------------------------------------------------------------------------------
+def suggest_host():
+    pass
+
+# ----------------------------------------------------------------------------------------------------------------------
+# plugin: 自动继承 struct，自动覆盖 phi0, rho0, rho
+
